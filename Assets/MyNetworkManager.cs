7b2ed3ab81cc689fc64443215ba83becf6ccf5b1
JensenHome\Nathan <nathanjensen.music@gmail.com>
2018-01-07 13:21:29 -0700

    Added Logs for the networking

---
+++ Assets/MyNetworkManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyNetworkManager : NetworkManager {
+
+	public void MyStartHost () {
+		StartHost();
+		Debug.Log (Time.timeSinceLevelLoad + " Starting Host");
+	}
+
+	public override void OnStartHost () {
+		Debug.Log (Time.timeSinceLevelLoad + " Started Host");
+	}
+
+	public override void OnStartClient (NetworkClient myClient) {
+		Debug.Log (Time.timeSinceLevelLoad + " " + myClient + " trying to log in.");
+	}
+
+	public override void OnClientConnect (NetworkConnection myConnection) {
+		Debug.Log (Time.timeSinceLevelLoad + " Client Connected to IP: " + myConnection.address);
+	}
+}
